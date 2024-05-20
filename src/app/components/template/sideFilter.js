287a78038cdb099e007d7918e17caf7d80d1b2d7
masproo22 <m.abbaszadeh2002@gmail.com>
2024-05-20 22:53:58 +0330

    side filter fix

--- src/app/components/template/sideFilter.js
+++ src/app/components/template/sideFilter.js
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-export default function SideFilter() {
+import Image from "next/image";
+
+export default function SideFilter({ active, setActive }) {
   const [value, setValue] = useState("female");
-  const [active, setActive] = useState(false);
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -18,29 +19,26 @@ export default function SideFilter() {
   return (
     <>
       <div
-        onClick={() => setActive((p) => (p = !p))}
         className={
           active
-            ? "md:hidden shadOne fixed top-[8rem] right-[21rem] w-[7rem] p-4 h-6 flex items-center justify-end bg-white"
-            : "md:hidden shadOne fixed top-[8rem] right-[-1rem] w-[7rem] p-4 h-6 flex items-center justify-end bg-white"
+            ? "w-[17rem] lg:w-96 gap-12 pl-12 h-screen flex flex-col pt-20 border-l fixed md:relative bg-white"
+            : "w-[17rem] lg:w-96 gap-12 pl-12 h-screen hidden pt-20 border-l relative bg-white md:flex md:flex-col"
         }
       >
-        مرتب سازی
-      </div>
-
-      <div
-        className={
-          active
-            ? "w-96 min-h-[62rem] flex pt-20 border-l relative  bg-white"
-            : "w-96 min-h-[62rem] hidden pt-20 border-l relative bg-white md:flex"
-        }
-      >
+        <Image
+          onClick={() => setActive((p) => !p)}
+          src={"/images/multiplication-svgrepo-com.svg"}
+          alt="exist"
+          width={50}
+          height={50}
+          className="w-8 h-w-8 self-end md:hidden"
+        ></Image>
         <RadioGroup
           aria-labelledby="demo-controlled-radio-buttons-group"
           name="controlled-radio-buttons-group"
           value={value}
           onChange={handleChange}
-          className="flex flex-col gap-3"
+          className="flex flex-col gap-3 w-[12rem] md:w-[20rem]"
         >
           <FormControlLabel
             className="mr-0"
--- src/app/products/[kind]/page.js
+++ src/app/products/[kind]/page.js
@@ -1,14 +1,48 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import SideFilter from "../../components/template/sideFilter";
 
 export default function products({ params }) {
   console.log(params.kind);
+  const [active, setActive] = useState(false);
+
   return (
     <div className="w-full flex">
-      <SideFilter />
+      <SideFilter setActive={setActive} active={active} />
 
-      <div className="w-full flex justify-center py-28">
-        <div className="grid grid-cols-3 gap-8">
+      <div className="w-full  flex flex-col items-end gap-40 py-[5rem] md:pr-14">
+        <div className=" w-full flex items-center justify-between md:justify-end gap-8">
+          <div
+            onClick={() => setActive((p) => !p)}
+            className="text-[1.3rem] text-second md:hidden w-24 h-12 bg-second/10 flex items-center justify-center rounded-md"
+          >
+            بر اساس
+          </div>
+          <div className="flex h-14 items-center justify-end gap-4 border-1 border-gray-800/20 pl-4 py-1">
+            <input
+              className="pr-4 h-full w-[20rem] sm:w-[30rem] text-[1.3rem] pl-2 focus:outline-none"
+              type="search"
+              placeholder="چه اموزشی مد نظرتان است؟..."
+            />
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-10 h-10"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8">
           <div className="w-[15rem] h-[10rem] bg-second"></div>
           <div className="w-[15rem] h-[10rem] bg-second"></div>
           <div className="w-[15rem] h-[10rem] bg-second"></div>
