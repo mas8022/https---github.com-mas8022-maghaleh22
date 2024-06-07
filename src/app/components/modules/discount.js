337c8776724e041e341c91ee172279cf7896e481
masproo22 <m.abbaszadeh2002@gmail.com>
2024-06-07 10:14:37 +0330

    fix discountTopbar module

--- src/app/components/modules/discount.js
+++ src/app/components/modules/discount.js
@@ -1,29 +1,47 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import { useState } from "react";
 
 const Discount = () => {
+  const [active, setActive] = useState(true);
+
   return (
-    <div className="w-full lgg:h-28 h-32 lg:mt-0 md:mt-8 mt-4 bg-second flex items-center justify-between px-[2.5rem] rounded-bl-3xl rounded-br-3xl">
-      <Image
-        src={"/images/discount-svgrepo-com.svg"}
-        width={100}
-        height={100}
-        alt="discount"
-        className="sm:size-20 size-16 xm:block hidden"
-      />
-      <p className="sm:text-[1.6rem] text-[1.3rem] text-first font-bold ">
-        تخفیفات ویژه 60% به مناسبت ماه رمضان
-      </p>
-      <button className="sm:size-14 size-10 bg-first/20 sm:rounded-3xl rounded-xl flex items-center justify-center active:scale-95">
-        <Image
-          src={"/images/multiplication-svgrepo-com.svg"}
-          width={100}
-          height={100}
-          alt="close"
-          className="sm:size-7 size-5"
-        />
-      </button>
+    <div
+      className={`${
+        !active && "hidden"
+      } w-full lgg:h-28 h-32 lg:mt-0 md:mt-8 mt-4`}
+    >
+      <div
+        className={`${
+          !active && "hidden"
+        } fixed w-full right-0 lgg:h-28 h-32 lg:mt-0 md:mt-8 mt-4 bg-second flex items-center justify-between px-[2.5rem] rounded-bl-full rounded-br-full`}
+      >
+        <div className="w-full px-6 sm:px-[5rem] md:px-[10rem] xl:px-[15rem] flex items-center justify-between">
+          <Image
+            src={"/images/discount-svgrepo-com.svg"}
+            width={100}
+            height={100}
+            alt="discount"
+            className="sm:size-20 size-16 xm:block hidden"
+          />
+          <p className="sm:text-[1.6rem] text-[1.3rem] text-first font-bold ">
+            تخفیفات ویژه 60% به مناسبت ماه رمضان
+          </p>
+          <button
+            onClick={() => setActive(false)}
+            className="sm:size-14 size-10 bg-first/20 sm:rounded-3xl rounded-xl flex items-center justify-center active:scale-95"
+          >
+            <Image
+              src={"/images/multiplication-svgrepo-com.svg"}
+              width={100}
+              height={100}
+              alt="close"
+              className="sm:size-7 size-5"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
