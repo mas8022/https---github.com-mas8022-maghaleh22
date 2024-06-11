bc3e006032c56138d723614d28ce34223ef70001
masproo22 <m.abbaszadeh2002@gmail.com>
2024-06-11 10:09:05 +0330

    fix 404

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,9 +4,11 @@ import MainSearch from "./components/template/mainSearch";
 
 const Page = () => {
   return (
-    <div className="w-full h-screen flex flex-col items-center">
-      <div className="w-full flex items-center justify-between">
-        <p className="text-[3rem] text-second drop-shadow-sm font-light">با عرض پوزش این صفحه وجود ندارد</p>
+    <div className="w-full h-screen flex flex-col items-center xm:pt-0 pt-20 xm:gap-0 gap-20 ">
+      <div className="w-full flex xm:flex-row flex-col-reverse items-center xm:justify-between">
+        <p className="md:text-[3rem] xm:text-[2.3rem] text-[2rem] text-second drop-shadow-sm font-light">
+          با عرض پوزش این صفحه وجود ندارد
+        </p>
         <Image
           src={"/images/404.png"}
           width={1000}
