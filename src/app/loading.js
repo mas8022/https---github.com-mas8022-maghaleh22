00c4be7e25a848dce5e3080523681b37c0060b87
masproo22 <m.abbaszadeh2002@gmail.com>
2024-07-25 17:42:36 +0330

    fix loading

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,7 +4,7 @@ import { MoonLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className="w-full h-[30rem] flex items-center justify-center bg-black/0">
+    <div className="w-full h-screen flex justify-center bg-black/0 pt-72">
       <MoonLoader size={50} color="#f97316" />
     </div>
   );
