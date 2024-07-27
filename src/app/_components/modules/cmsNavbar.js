a771b36aa22e02420da34d7f62ec976c5e678be9
masproo22 <m.abbaszadeh2002@gmail.com>
2024-07-27 19:55:06 +0330

    fix

--- src/app/_components/modules/cmsNavbar.js
+++ src/app/_components/modules/cmsNavbar.js
@@ -134,6 +134,25 @@ const CmsNavbar = () => {
                   تایید نویسندگان
                 </div>
               </Link>
+              <Link href="/" className={`active:bg-black/10`}>
+                <div className="flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="sm:size-10 size-7"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                    />
+                  </svg>
+                  بازگشت
+                </div>
+              </Link>
             </ul>
           </div>
         </Side>
