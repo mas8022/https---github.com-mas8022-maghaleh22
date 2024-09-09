501c9fd0c44fa963c04b887ffb6bc52c247fa130
masproo22 <m.abbaszadeh2002@gmail.com>
2024-09-09 12:00:28 +0330

    fix

--- configs/db.js
+++ configs/db.js
@@ -5,7 +5,9 @@ function connectToDb() {
     if (mongoose.connections[0].readyState) {
       return false;
     } else {
-      mongoose.connect("mongodb://localhost:27017/maghaleh");
+      mongoose.connect(
+        "mongodb://root:eNLx53XD5qqNBdm3uchTEkq4@fitz-roy.liara.cloud:32074/my-app?authSource=admin"
+      );
     }
   } catch (error) {
     return Response.json({ message: "don`t connect to db" });
--- src/app/cms/cmsNotif/page.js
+++ src/app/cms/cmsNotif/page.js
@@ -1,7 +1,9 @@
 import React from "react";
 import CmsNotifCart from "../../_components/modules/cmsNotifCart";
 import contactUsMessageModel from "@/models/contactUsMessage";
+import connectToDb from "@/configs/db";
 const page = async () => {
+  connectToDb();
   const comments = await contactUsMessageModel.find(
     { isAnswer: false },
     "fullName email phone message"
@@ -9,7 +11,6 @@ const page = async () => {
 
   return (
     <div>
-   
       <div className="w-full  flex flex-col items-end gap-40 py-[5rem] md:pr-14">
         <div className="w-full flex justify-center">
           <div className="grid grid-cols-1 lgg:grid-cols-2  2xl:grid-cols-3 gap-8">
